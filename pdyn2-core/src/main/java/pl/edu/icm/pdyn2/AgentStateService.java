4b672945605f598da7264bfbb70afe18c198c4ee
Bartosz Krupa <bk437917@icm.edu.pl>
2023-11-16 12:26:10 +0100

    Feature/452 create output system

--- pdyn2-core/src/main/java/pl/edu/icm/pdyn2/AgentStateService.java
+++ pdyn2-core/src/main/java/pl/edu/icm/pdyn2/AgentStateService.java
@@ -236,7 +236,9 @@ public class AgentStateService {
     public void addSourcesDistribution(Entity agent, SoftEnumDiscretePDF<ContextInfectivityClass> exposurePerContext) {
         var immunizationSources = agent.getOrCreate(ImmunizationSources.class);
         var immunizationSource = new ImmunizationSource();
-        exposurePerContext.normalize();
+        if (!exposurePerContext.isEmpty()) {
+            exposurePerContext.normalize();
+        }
         for (var contextType : contextInfectivityClasses.values()) {
             immunizationSource.setForContextType(contextInfectivityClasses, contextType, exposurePerContext.get(contextType));
         }
